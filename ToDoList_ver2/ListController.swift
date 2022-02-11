7b86ab3eec5072d7ce207e02b7fd8a1749d553d3
Kim-Junhwan <tazanboy1@naver.com>
2022-02-11 23:48:27 +0900

    register, login , list page

---
+++ ToDoList_ver2/ListController.swift
@@ -0,0 +1,13 @@
+//
+//  ListController.swift
+//  ToDoList_ver2
+//
+//  Created by JunHwan Kim on 2022/02/11.
+//
+
+import Foundation
+import UIKit
+
+class ListController : UIViewController{
+    
+}
--- ToDoList_ver2/RegisterController.swift
+++ ToDoList_ver2/RegisterController.swift
@@ -10,4 +10,18 @@ import UIKit
 
 class RegisterController : UIViewController{
     
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    @IBAction func registerPressed(_ sender: UIButton) {
+        print("Register")
+    }
+    
 }
